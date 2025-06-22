eb5bdcac536a274011e52a9b7d61317d2acfaaf4
Jaimito27 <softjaimito27@gmail.com>
2025-06-22 01:04:17 -0500

    Primer commit donde se instala bulma, ngxs y se configura el app config con los parametros de ngxs para lectura de los estados

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore([],
+      withNgxsReduxDevtoolsPlugin({
+        disabled: !isDevMode()
+      }),
+      withNgxsLoggerPlugin({
+        disabled: !isDevMode()
+      }),
+      withNgxsStoragePlugin({
+        keys: ['auth.token', 'auth.user'],
+      })
+    )
   ]
 };
