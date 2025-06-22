f3e9317cf9134b9c26b862757c9060ba43ee2939
Jaimito27 <softjaimito27@gmail.com>
2025-06-22 01:29:49 -0500

    Se establecen las rutas para las que hasta el momento serán las direcciones del proyecto con lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: ()=> import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+  {
+    path: 'dashboard',
+   loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
+  },
+  {
+    path: 'users',
+   loadChildren: () => import('./features/users/users.routes').then(m => m.USERS_ROUTES)
+  },
+  {
+    path: 'documents',
+   loadChildren: () => import('./features/documents/documents.routes').then(m => m.DOCUMENTS_ROUTES)
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
---
+++ src/app/features/documents/documents.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { Documents } from "./documents";
+
+export const DOCUMENTS_ROUTES: Routes = [
+  {path: 'documents', component: Documents}
+]
